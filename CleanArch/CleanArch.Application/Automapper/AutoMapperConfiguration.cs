95c1ef1f1692c80bd0eb3e9cd95dcdd92647a69d
Mike Rothschilds <mrothschilds@live.com>
2021-09-08 16:16:03 -0400

    autoMapper configuration and refactoring

---
+++ CleanArch/CleanArch.Application/Automapper/AutoMapperConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace CleanArch.Application.Automapper
+{
+    public class AutoMapperConfiguration
+    {
+        public static MapperConfiguration RegisterMappings()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new ViewModelToDomainProfile());
+                cfg.AddProfile(new DomainToViewModelProfile());
+            });
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Application/Automapper/DomainToViewModelProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Models;
+
+namespace CleanArch.Application.Automapper
+{
+   public class DomainToViewModelProfile : Profile
+    {
+        public DomainToViewModelProfile()
+        {
+            CreateMap<Course, CourseViewModel>();
+        }
+    }
+}
